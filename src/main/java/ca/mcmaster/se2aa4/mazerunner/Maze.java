3c3d32f3f74846520454fd36326f6600bece7e03
Viraj <340viraj@gmail.com>
2025-01-24 20:29:56 -0500

    Implemented Direction and Point

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -18,12 +18,23 @@ enum Direction {
     UP, RIGHT, DOWN, LEFT;
 
     public Direction turnRight() {
-        return null;
-
+        switch (this) {
+            case UP: return RIGHT;
+            case RIGHT: return DOWN;
+            case DOWN: return LEFT;
+            case LEFT: return UP;
+        }
+        return this;
     }
 
     public Direction turnLeft() {
-        return null;
+        switch (this) {
+            case UP: return LEFT;
+            case LEFT: return DOWN;
+            case DOWN: return RIGHT;
+            case RIGHT: return UP;
+        }
+        return this;
     }
 }
 
